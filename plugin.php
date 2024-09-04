65558bef8d4e803444cb4a25c4f6a50a48fd9872
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-09-04 23:46:08 +0200

    Version 9.0

--- plugin.php
+++ plugin.php
@@ -23,8 +23,8 @@ $id = 'xaic';
 
 $version = '2.0.0';
 
-$ilias_min_version = '8.0';
-$ilias_max_version = '8.999';
+$ilias_min_version = '9.0';
+$ilias_max_version = '9.999';
 
 $responsible = 'Jesus Copado';
 $responsible_mail = 'jcopado@surlabs.com';
