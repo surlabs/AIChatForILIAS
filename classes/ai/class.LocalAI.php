b904665201612615fd20ae678ecf816e84f88e6d
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-09-20 12:21:42 +0200

    Version 7.0.2 (Local llama models)

---
+++ classes/ai/class.LocalAI.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types=1);
+
+namespace ai;
+
+use ilObjAIChatGUI;
+use objects\Chat;
+use platform\AIChatConfig;
+use platform\AIChatException;
+
+class LocalAI extends LLM
+{
+    private string $model;
+    private string $apiKey;
+
+    public function __construct(string $model)
+    {
+        $this->model = $model;
+    }
+
+    public function setApiKey(string $apiKey): void
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @throws AIChatException
+     */
+    public function sendChat(Chat $chat)
+    {
+        global $DIC;
+
+        $apiUrl = 'http://ki:8080/v1/chat/completions';
+        $streaming = boolval(AIChatConfig::get("streaming_enabled")) ?? false;
+
+        $payload = json_encode([
+            "messages" => $this->chatToMessagesArray($chat),
+            "model" => $this->model,
+            "temperature" => 0.5,
+            "stream" => $streaming
+        ]);
+
+        $curlSession = curl_init();
+
+        curl_setopt($curlSession, CURLOPT_URL, $apiUrl);
+        curl_setopt($curlSession, CURLOPT_POST, true);
+        curl_setopt($curlSession, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, !$streaming);
+        curl_setopt($curlSession, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $this->apiKey
+        ]);
+
+        if (\ilProxySettings::_getInstance()->isActive()) {
+            $proxyHost = \ilProxySettings::_getInstance()->getHost();
+            $proxyPort = \ilProxySettings::_getInstance()->getPort();
+            $proxyURL = $proxyHost . ":" . $proxyPort;
+            curl_setopt($curlSession, CURLOPT_PROXY, $proxyURL);
+        }
+
+        $responseContent = '';
+
+        if ($streaming) {
+            curl_setopt($curlSession, CURLOPT_WRITEFUNCTION, function ($curlSession, $chunk) use (&$responseContent) {
+                $responseContent .= $chunk;
+                echo $chunk;
+                ob_flush();
+                flush();
+                return strlen($chunk);
+            });
+        }
+
+        $response = curl_exec($curlSession);
+        $httpcode = curl_getinfo($curlSession, CURLINFO_HTTP_CODE);
+        $errNo = curl_errno($curlSession);
+        $errMsg = curl_error($curlSession);
+        curl_close($curlSession);
+
+        if ($errNo) {
+            ilObjAIChatGUI::sendApiResponse(array("error" => $DIC->language()->txt("rep_robj_xaic_error_http_openai") . ": " . $errMsg), 500);
+        }
+
+        if ($httpcode != 200) {
+            if ($httpcode === 401) {
+                ilObjAIChatGUI::sendApiResponse(array("error" => $DIC->language()->txt("rep_robj_xaic_error_apikey")), 401);
+            } else {
+                ilObjAIChatGUI::sendApiResponse(array("error" => $DIC->language()->txt("rep_robj_xaic_error_http_openai")), $httpcode);
+            }
+        }
+
+        if (!$streaming) {
+            $decodedResponse = json_decode($response, true);
+            return $decodedResponse['choices'][0]['message']['content'] ?? "";
+        }
+
+        // Procesar la respuesta completa acumulada en $responseContent
+        $messages = explode("\n", $responseContent);
+        $completeMessage = '';
+
+        foreach ($messages as $message) {
+            if (trim($message) !== '' && strpos($message, 'data: ') === 0) {
+                $json = json_decode(substr($message, strlen('data: ')), true);
+                if ($json && isset($json['choices'][0]['delta']['content'])) {
+                    $completeMessage .= $json['choices'][0]['delta']['content'];
+                }
+            }
+        }
+
+        return $completeMessage;
+    }
+}
--- plugin.php
+++ plugin.php
@@ -21,7 +21,7 @@
 
 $id = 'xaic';
 
-$version = '7.0.1';
+$version = '7.0.2';
 
 $ilias_min_version = '7.0';
 $ilias_max_version = '7.999';
